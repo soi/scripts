08b1ce32fc11231c5b4ae18fb8da883d5d25da28
Felix Stiehler <felix.stiehler87@gmail.com>
2017-08-23 09:37:07 +0200

    ipython_startup.py

--- ipython_startup.py
+++ ipython_startup.py
@@ -14,7 +14,7 @@ except ImportError:
 plt.interactive(True)
 
 np.set_printoptions(linewidth = 75)
-np.set_printoptions(threshold=500)
+np.set_printoptions(threshold=np.inf)
 
 a = np.arange(10)
 b = np.arange(100)
