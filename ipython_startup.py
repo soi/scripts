0a67061c6fa67421d8d7d65d543a0408c06784ec
Felix Stiehler <felix.stiehler87@gmail.com>
2018-04-26 17:02:43 +0200

    ipython_startup.py

--- ipython_startup.py
+++ ipython_startup.py
@@ -1,15 +1,9 @@
 import sys, os
 import numpy as np
-# import cv2
 import matplotlib.pyplot as plt
-# import Tkinter
 from pprint import pprint
 import sklearn
-
-try:
-    import _pickle as pickle #python2
-except ImportError:
-    import pickle
+import pickle
 
 plt.interactive(True)
 
