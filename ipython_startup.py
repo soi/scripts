fc7e2bd8035a1149387f1124963b3f1ba01247b1
Felix Stiehler <felix.stiehler87@gmail.com>
2018-08-20 13:33:27 +0200

    ipython_startup.py

--- ipython_startup.py
+++ ipython_startup.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pprint import pprint
 import sklearn
-import pickle
+import h5py
 
 plt.interactive(True)
 
@@ -13,3 +13,5 @@ np.set_printoptions(threshold=np.inf)
 a = np.arange(10)
 b = np.arange(100)
 d = {'eins': 1, 'zwei': 2}
+
+f = h5py.File('/home/felix/git/PairwiseMHCRanking/data_local/HLA_A_02_01.h5', 'r')
