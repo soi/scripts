e614eff77495ad34531794aa7de4a1bcd6588759
Felix Stiehler <felix.stiehler87@gmail.com>
2019-03-29 09:48:03 +0100

    ipython_startup.py

--- ipython_startup.py
+++ ipython_startup.py
@@ -14,6 +14,3 @@ np.set_printoptions(threshold=np.inf)
 a = np.arange(10)
 b = np.arange(100)
 d = {'eins': 1, 'zwei': 2}
-
-AMINO_ACIDS = '-ACDEFGHIKLMNPQRSTVWY'
-ctable = CharacterTable(AMINO_ACIDS)
