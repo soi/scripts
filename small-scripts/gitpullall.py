55b199a1823f43e1ac87b4af51bb074ca1f83bcf
Felix Stiehler <felix.stiehler87@gmail.com>
2019-03-31 13:53:27 +0200

    gitpua update for new hhu job

--- small-scripts/gitpullall.py
+++ small-scripts/gitpullall.py
@@ -15,16 +15,18 @@ def pull_or_clone_git(git):
         args = shlex.split('git clone ' + git[1] + ' ' + git[0])
         Popen(args).communicate()
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 2e:
     USER = getpass.getuser()
 else:
     USER = sys.argv[1]
 
 GIT_HOME_DIR = '/home/' + USER + '/git/'
 GIT_LIST = [
-    (GIT_HOME_DIR + 'phd', 'git@github.com:soi/phd.git'),
-    (GIT_HOME_DIR + 'MHCPredictions', 'git@github.com:DiltheyLab/MHCPredictions.git'),
-    (GIT_HOME_DIR + 'PairwiseMHCRanking', 'git@github.com:DiltheyLab/PairwiseMHCRanking.git'),
+    # (GIT_HOME_DIR + 'phd', 'git@github.com:soi/phd.git'),
+    # (GIT_HOME_DIR + 'MHCPredictions', 'git@github.com:DiltheyLab/MHCPredictions.git'),
+    # (GIT_HOME_DIR + 'PairwiseMHCRanking', 'git@github.com:DiltheyLab/PairwiseMHCRanking.git'),
+    (GIT_HOME_DIR + 'HelixerPrep', 'git@github.com:soi/HelixerPrep.git'),
+    (GIT_HOME_DIR + 'GeenuFF', 'git@github.com:soi/GeenuFF.git'),
     (GIT_HOME_DIR + 'scripts', 'git@github.com:soi/scripts.git'),
     (GIT_HOME_DIR + 'misc', 'git@github.com:soi/misc.git')
 ]
