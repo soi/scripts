1a6b81bfcbd3574066656eb91a1166d5615497c5
Felix Stiehler <felix.stiehler87@gmail.com>
2020-05-19 17:56:43 +0200

    .bash_aliases ipython_startup.py

--- ipython_startup.py
+++ ipython_startup.py
@@ -1,16 +1,16 @@
-import sys, os
-import numpy as np
-import matplotlib.pyplot as plt
-from pprint import pprint
-import random
-import math
-import h5py
-
-plt.interactive(True)
-
-# np.set_printoptions(linewidth = 75)
-np.set_printoptions(threshold=np.inf)
-
-a = np.arange(10)
-b = np.arange(100)
-d = {'eins': 1, 'zwei': 2}
+# import sys, os
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from pprint import pprint
+# import random
+# import math
+# import h5py
+#
+# plt.interactive(True)
+#
+# # np.set_printoptions(linewidth = 75)
+# np.set_printoptions(threshold=np.inf)
+#
+# a = np.arange(10)
+# b = np.arange(100)
+# d = {'eins': 1, 'zwei': 2}
