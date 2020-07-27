2fac24b44219167541959b32378890e5395d1fbf
Felix Stiehler <felix.stiehler87@gmail.com>
2020-07-27 14:08:36 +0200

    comment

--- small-scripts/setup-new-pc.sh
+++ small-scripts/setup-new-pc.sh
@@ -4,7 +4,7 @@
 ./gitpullall.py
 sudo install-packages/install_packages desktop
 sudo apt-get autoremove
-./pip_install
+# ./pip_install
 ./setup-vim.sh
 
 ./cp_scripts.py -m from-git
@@ -12,5 +12,6 @@ sudo apt-get autoremove
 dconf write /org/gnome/terminal/legacy/keybindings/next-tab "'<Shift>Right'"
 dconf write /org/gnome/terminal/legacy/keybindings/prev-tab "'<Shift>Left'"
 
+echo -e "\nyou may want to run pip_install or install stuff via conda"
 echo "maybe also run sudo apt-get remove firefox thunderbird rhythmbox"
 echo "have fun:)"
