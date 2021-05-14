8a159fbb64c64c73e1a4f9884ebd1a0ff3d631a9
Felix Stiehler <felix.stiehler87@gmail.com>
2021-05-14 13:55:36 +0200

    ipython_startup.py

--- ipython_startup.py
+++ ipython_startup.py
@@ -12,5 +12,6 @@ plt.interactive(True)
 np.set_printoptions(threshold=np.inf)
 
 a = np.arange(10)
-b = np.arange(100)
+b = np.arange(100).reshape(10, 10)
+c = np.arange(1000).reshape(10, 10, 10)
 d = {'eins': 1, 'zwei': 2}
