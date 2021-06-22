037e7be68abca186431cd41d8a80d3fa678104e7
Felix Stiehler <felix.stiehler87@gmail.com>
2021-06-22 17:29:58 +0200

    .bash_aliases

--- small-scripts/gitpullall.py
+++ small-scripts/gitpullall.py
@@ -43,6 +43,7 @@ if args.work:
         (git_home_dir + '2020-06-kit-atmospheric-chemistry', 'git@gitlab.dkrz.de:aim/2020-06-kit-atmospheric-chemistry.git'),
         (git_home_dir + '2020-11-hzg-rogue-waves', 'git@gitlab.dkrz.de:aim/2020-11-hzg-rogue-waves.git'),
         (git_home_dir + '2021-07-gfz-marine-litter', 'git@gitlab.dkrz.de:aim/2021-07-gfz-marine-litter.git'),
+        (git_home_dir + 'trace.detection', 'git@git-prem.gfz-potsdam.de:trace/trace.detection.git'),
         (git_home_dir + 'ml-notebook-collection', 'git@gitlab.dkrz.de:aim/ml-notebook-collection.git'),
         (git_home_dir + 'aimtorch', 'git@gitlab.dkrz.de:aim/aimtorch.git'),
     ]
