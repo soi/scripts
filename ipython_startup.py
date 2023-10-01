7ec5e7d526f1a817ea7038e04e081567d358b9f4
Felix Stiehler <felix.stiehler87@gmail.com>
2023-10-01 20:31:33 +0200

    ipython_startup.py

--- ipython_startup.py
+++ ipython_startup.py
@@ -1,12 +1,8 @@
 import sys, os
 import numpy as np
-import matplotlib.pyplot as plt
 from pprint import pprint
 import random
 import math
-import h5py
-
-plt.interactive(True)
 
 # np.set_printoptions(linewidth = 75)
 np.set_printoptions(threshold=np.inf)
